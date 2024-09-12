c65f008d6674251b0e63b1031c247e10770853c3
Pedro Segatto <pedrobsegatto@gmail.com>
2024-09-12 15:45:12 -0300

    Entrega01

--- caminhao.py
+++ caminhao.py
@@ -1,11 +1,10 @@
 from veiculo import Veiculo
 
 class Caminhao(Veiculo):
-    def __init__(self, marca, modelo, placa, ano, n_portas, comprimento, capacidade):
+    def __init__(self, marca, modelo, placa, ano, n_portas, comprimento):
         super().__init__(marca, modelo, placa,ano)
         self.__n_portas = n_portas
         self.__comprimento = comprimento
-        self.__capacidade = capacidade
     #Sobrescrita do método __str__()
     def __str__(self):
         #Invoco o método __str__() da SUPERCLASSE (Veiculo)
@@ -16,5 +15,4 @@ class Caminhao(Veiculo):
         return f'''{retorno}
  - N_portas: {self.__n_portas}
  - Comprimento: {self.__comprimento}
- - Capacidade: {self.__capacidade}
 '''
